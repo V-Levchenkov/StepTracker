73b0b4608f8e16c6dbbba6947cd5519503be0a47
V.levchenkov <V.levchenkovv@gmail.com>
2022-01-30 14:03:13 +0300

    ПЕрвый день

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Praktikum {
+public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -10,27 +10,29 @@ public class Praktikum {
             int command = scanner.nextInt();
 
             if (command == 1) {
-
+                System.out.println("Введите необходимый день");
+                int day = scanner.nextInt();
+                System.out.println("Выберете месяц");
+                int month = scanner.nextInt();
+//Ввести количество шагов за определённый день;
             } else if (command == 2) {
-
+//Напечатать статистику за определённый месяц;
             } else if (command == 3) {
-
+//Изменить цель по количеству шагов в день;
             } else if (command == 4) {
+       //Выйти из приложения.
                 break;
-            } else if (command == 5) {
-
-
-            } else {
+            }  else {
                 System.out.println("Введена неверная команда");
             }
         }
     }
 
     public static void printMenu() {
-        System.out.println("Что вы хотите сделать? ");
-        System.out.println("1 - Конвертировать валюту");
-        System.out.println("2 - Получить совет");
-        System.out.println("3 - Ввести трату");
-        System.out.println("4 - Показать траты по категориям");
+        System.out.println("Выберите действие :");
+        System.out.println("1 - Внести количество шагов за определенный день");
+        System.out.println("2 - Напечатать статистику за определенный месяц");
+        System.out.println("3 - Изменить цель по количеству шагов в день");
+        System.out.println("4 - Выйти из приложения");
     }
 }
