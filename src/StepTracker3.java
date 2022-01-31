a4d3e7db9589f1a12b7dd4823cf1a63499c7b884
V.levchenkov <V.levchenkovv@gmail.com>
2022-01-31 21:19:11 +0300

    Второй день

--- src/StepTracker3.java
+++ src/StepTracker3.java
@@ -1,2 +1,16 @@
-public class StepTracker3 {
+import java.util.HashMap;
+
+public class StepTracker {
+
+    HashMap<Integer, MonthData> monthToData = new HashMap<Integer, MonthData>();
+
+    public StepTracker() {
+        for (int i = 0; i < 12; i++) {
+            monthToData.put(i, new MonthData());
+        }
+    }
+
+    class MonthData {
+        // Заполните класс самостоятельно
+    }
 }
